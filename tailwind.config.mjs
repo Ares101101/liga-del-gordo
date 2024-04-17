ecad47119454862b0bbb5e7d9b7440fb1bebaae9
Ares <diego.amachiflores@gmail.com>
2024-04-17 17:01:12 -0500

    17/04/2024

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,18 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				atomic: ["Atomic", "cursive"],
+			},
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-secondary)",
+				accent: "var(--color-accent)",
+				twitch: "var(--color-twitch)",
+				ice: "var(--color-twitch-ice)",
+			},
+		},
 	},
 	plugins: [animations],
 }
